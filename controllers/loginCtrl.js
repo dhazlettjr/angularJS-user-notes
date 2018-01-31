28f5ef60f917934f8562a1c8e8c09dcc8e51a760
dhazlettjr <d.hazlett.jr@gmail.com>
2018-01-31 15:47:41 -0600

    created login controller for auth

--- controllers/loginCtrl.js
+++ controllers/loginCtrl.js
@@ -1,6 +1,37 @@
-'use strict';
+"use strict";
 
-// this will set up the controller for the project
-angular.module("user").controller("loginCtrl", function($scope) {
+angular
+  .module("user")
+  .controller("loginCtrl", function($scope, authFactory, $window, $rootScope) {
+    $scope.test = "hello";
 
-});
+    // TODO: Grab the user form info and send it to AuthFactory
+    // for register / login / logout -- hmm. Maybe rename this Ctrl?
+    $scope.register = () => {
+      authFactory.createUser($scope.account).then(user => {
+        console.log("newUser", user);
+        $scope.login();
+      })
+      .catch(function({code, message}) {
+        console.log("Oops", code, message);
+      });
+    };
+
+    $scope.login = () => {
+      authFactory.loginUser($scope.account).then(user => {
+        console.log("logged in user", user);
+        $window.location.href = "#!/notes";
+      })
+      .catch( (err) => {
+        console.log(err);
+      });
+    };
+
+    $scope.logout = () => {
+      authFactory.logoutUser()
+      .then( (data) => {
+        console.log("logged out", data);
+      });
+    };
+    // $scope.logout();
+  });
