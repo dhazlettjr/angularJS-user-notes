99d2197b4cdc44ab8ec17508f1d3b018dfddc90a
dhazlettjr <d.hazlett.jr@gmail.com>
2018-01-31 14:11:13 -0600

    created controllers for register, view, new, login

--- controllers/noteListCtrl.js
+++ controllers/noteListCtrl.js
@@ -5,10 +5,9 @@ angular.module("user").controller("noteListCtrl", function($scope, notesFactory)
   notesFactory.getNotesData()
   .then( (noteData) => {
       console.log("hey",noteData)
+
+      //object.entries is a simple way to push an object into an array
       $scope.noteArray = Object.entries(noteData.data);
-    //   for (let notes in noteData){
-    //       $scope.noteArray.push(noteData[notes]);
-    //   }
      console.log(noteData.data);
 
   });
